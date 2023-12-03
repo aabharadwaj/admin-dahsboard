7974faf6a58f94697e2858073af5094b786f608e
Pradeept <pradhy09@gmail.com>
2023-12-03 22:39:43 +0530

    admin dashboard code added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Admin from './components/Admin';
+import { useEffect } from 'react';
+
+function App() {
+  return (
+    <div>
+      <Admin />
+    </div>
+  );
+}
+
+export default App;
